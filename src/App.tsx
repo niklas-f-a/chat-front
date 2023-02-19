f43687759a6d981d92da8b62e3a8d310e30fe5f4
Niklas Andersson <niklas.andersson@iths.se>
2023-02-19 17:55:55 +0100

    Add axios & testing stuff

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,56 @@
-import { useState } from 'react'
+import { SyntheticEvent, useEffect, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import './App.css'
+import axios from 'axios'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+
+  const [some, setSome] = useState<any>(null)
+
+  const [isLoginForm, setIsLoginForm] = useState(true)
+
+  useEffect(() => {
+    if(!some) return
+    // socket enter
+  }, [some])
+
+  const onLogin = (e: SyntheticEvent) => {
+    e.preventDefault()
+    setSome('ähej')
+  }
+
+  const onSignIn = (e: SyntheticEvent) => {
+    e.preventDefault()
+    axios.post('http://127.0.0.1:5001/api/v1/auth/signup', { email, password }, { withCredentials: true })
+      .then(res => {
+        console.log(res)
+        setSome(res.data)
+      })
+  }
+
+  const sendMessage = () => {
+
+  }
+
+  const toggleForm = () => setIsLoginForm(currentForm => !currentForm)
 
   return (
     <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <button onClick={toggleForm}>{isLoginForm ? 'sign up' : 'to log in'}</button>
+      <form onSubmit={isLoginForm ? onLogin : onSignIn}>
+        <input value={email} onChange={(e) => setEmail(e.target.value)} type="text" placeholder='email' />
+        <input value={password} onChange={e => setPassword(e.target.value)} type="password" placeholder='*******' />
+        <button type='submit'>Send</button>
+      </form>
+      <div>{some && JSON.stringify(some)}</div>
+      {!!some && (
+        <form onSubmit={sendMessage}>
+          <textarea name="message" id="message" cols={30} rows={10}></textarea>
+          <button type='submit'>send message</button>
+        </form>
+      )}
     </div>
   )
 }
